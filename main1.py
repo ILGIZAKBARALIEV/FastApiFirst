2fa5d45914ba7782ab08483e0f64469bf4657a1d
ILGIZ_AKBARALIEV <okoo.goe@gmail.com>
2026-03-09 16:50:22 +0600

    seccond_work

--- main1.py
+++ main1.py
@@ -1,6 +1,9 @@
 from  fastapi import FastAPI
-
+import uvicorn
 app = FastAPI()
-@app.get("/")
+@app.get("/",summary='Главная страница', tags=['Оснавная страница '])
 def root():
-    return {"message": "Hello World"}
+    return {"message": "Привет мир"}
+
+if __name__ == "__main__":
+    uvicorn.run('main1:app',reload=True)
